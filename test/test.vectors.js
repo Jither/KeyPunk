3b0cee7f08355bde9a61bd390648550b47f0a511
Jimmi Thøgersen <jimmith@gmail.com>
2014-04-22 02:47:11 +0200

    Lint fixes in test vector test runner

--- test/test.vectors.js
+++ test/test.vectors.js
@@ -1,7 +1,18 @@
+/* global keypunk, test, equal */
+
 (function() {
-	function fileChanged()
+	"use strict";
+
+	var settings = {
+		useKdf: false
+	};
+
+	var fileInput = $("#file");
+	fileInput.change(fileChanged);
+
+	function fileChanged(e)
 	{
-		var file = this.files[0];
+		var file = e.currentTarget.files[0];
 		var reader = new FileReader();
 		reader.onload = fileRead;
 		reader.readAsText(file);
@@ -14,67 +25,59 @@
 		startTestCollection(vectorCollection);
 	}
 
-	var fileInput = $("#file");
-	fileInput.change(fileChanged);
-}());
+	function startTestCollection(vectorCollection)
+	{
+		var alphabets = vectorCollection.alphabets;
+		var testSets = vectorCollection.testSets;
 
-var settings = {
-	useKdf: false
-};
-
-function startTestCollection(vectorCollection)
-{
-	var alphabets = vectorCollection.alphabets;
-	var testSets = vectorCollection.testSets;
-
-	testSets.forEach(function(testSet) {
-		startTestSet(testSet, alphabets);
-	});
-}
-
-function profileToString(profile)
-{
-	return keypunk.utils.format("{0}{1} - alphabet: '{2}', useAllSets: {3}, modifier: {4}, passwordLength: {5}", 
-		profile.algorithm.name,
-		profile.algorithm.hmac ? " (HMAC)" : "",
-		profile.alphabet.join(""),
-		profile.useAllSets,
-		profile.modifier,
-		profile.passwordLength
-	);
-}
-
-function startTestSet(testSet, alphabets)
-{
-	var tvProfile = testSet.profile;
-	var tests = testSet.tests;
-
-	var profile = {
-		algorithm: {
-			name: tvProfile.algo,
-			hmac: tvProfile.hmac
-		},
-		alphabet: alphabets[tvProfile.alph],
-		useAllSets: tvProfile.uas,
-		modifier: tvProfile.mod,
-		passwordLength: tvProfile.len
-	};
-
-	startTest(profile, tests);
-}
-
-function startTest(profile, tests)
-{
-	test(profileToString(profile), function() {
-		tests.forEach(function(testItem)
-		{
-			var mpw = testItem.mpw;
-			var vectors = testItem.vectors;
-			for (var input in vectors)
-			{
-				equal(keypunk.core.generate(mpw, input, settings, profile), vectors[input], "master password: '" + mpw + "', input: '" + input + "'");
-			}
+		testSets.forEach(function(testSet) {
+			startTestSet(testSet, alphabets);
 		});
-	});
+	}
 
-}
+	function profileToString(profile)
+	{
+		return keypunk.utils.format("{0}{1} - alphabet: '{2}', useAllSets: {3}, modifier: {4}, passwordLength: {5}", 
+			profile.algorithm.name,
+			profile.algorithm.hmac ? " (HMAC)" : "",
+			profile.alphabet.join(""),
+			profile.useAllSets,
+			profile.modifier,
+			profile.passwordLength
+		);
+	}
+
+	function startTestSet(testSet, alphabets)
+	{
+		var tvProfile = testSet.profile;
+		var tests = testSet.tests;
+
+		var profile = {
+			algorithm: {
+				name: tvProfile.algo,
+				hmac: tvProfile.hmac
+			},
+			alphabet: alphabets[tvProfile.alph],
+			useAllSets: tvProfile.uas,
+			modifier: tvProfile.mod,
+			passwordLength: tvProfile.len
+		};
+
+		startTest(profile, tests);
+	}
+
+	function startTest(profile, tests)
+	{
+		test(profileToString(profile), function() {
+			tests.forEach(function(testItem)
+			{
+				var mpw = testItem.mpw;
+				var vectors = testItem.vectors;
+				for (var input in vectors)
+				{
+					equal(keypunk.core.generate(mpw, input, settings, profile), vectors[input], "master password: '" + mpw + "', input: '" + input + "'");
+				}
+			});
+		});
+	}	
+}());
